082cfb4b64eebe9dfa1ef6622f8dad5244d1ab1e
Angelo <angeloquispesuarez@gmail.com>
2024-09-25 18:35:40 -0500

    Problemas hechos en clase

--- lab_1_practica/problema14.cpp
+++ lab_1_practica/problema14.cpp
@@ -6,3 +6,25 @@
 // metros cúbicos y en pies cúbicos. Considere los siguientes factores de conversión:
 // 1 pie cúbico = 0.0283 metros cúbicos, 1 metro cúbico = 1000 litros, 1 yarda cúbica = 27 pies
 // cúbicos.
+#include<iostream>
+using namespace std;
+int main (){
+    float c1,c2,total;
+    //covercion de litras a m3 and pies
+    const float yardas_mcub = 0.76,metros_yardas = 35;
+    const float litros_c1=0.001,pies_c2=0.037;
+    //coversion de pies cub a m3 and pies
+    // const float litros_c2=pies_c2=;
+    cout<<"Ingrese la capacidad del tanque 1 en litros: ";cin>>c1;
+    cout<<"Ingrese la capacidad del tanque 2 en yadas cubicas: ";cin>>c2;
+   //cant de total
+    total = c1*litros_c1+ c2*yardas_mcub;
+    cout<<"El total en metros cubicos es: "<<total<<endl;
+    cout<<"El total en yardas cubicas es: "<<total*35<<endl;
+   //c1==litros
+    cout<<"Cantidad de agua reservada para riesgos  de tanque 1 es: "<<c1*litros_c1*0.25<<"m3  y "<< c1*litros_c1*metros_yardas*0.25<<"pies "<<endl;
+    cout<<"Cantidad de agua reservada para uso domestico de tanque 1 es: "<<c1*litros_c1*0.75<<"m3 y "<< c1*litros_c1*metros_yardas*0.75<<"pies "<<endl;
+   //c2=yardas
+    cout<<"Cantidad de agua reservada para riesgos  de tanque 2 es: "<<c2*yardas_mcub*0.25<<"m3 y "<<c2*pies_c2*0.25<<"pies "<<endl;
+    cout<<"Cantidad de agua reservada para uso domestico de tanque 2 es: "<<c2*yardas_mcub*0.75<<"m3 y "<<c2*pies_c2*.75<<"pies "<<endl;
+}
--- lab_1_practica/problema15.cpp
+++ lab_1_practica/problema15.cpp
@@ -3,3 +3,33 @@
 // considera fracción de hora cuando haya pasado de 5 minutos. Diseñe un programa que
 // determine cuánto debe pagar un cliente por su estacionamiento en un solo día de la semana. Si
 // el tiempo ingresado es incorrecto imprima un mensaje de error.
+#include<iostream>
+using namespace std;
+int main (){
+    string dias[7]= {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
+    string dia;
+    int hora,min,extra,total;
+    float c_u;
+    cout<<"En que dia se encuentra: ";cin>>dia;
+    if(dias[0]==dia || dias[1]==dia){
+        c_u=2;
+        cout<<"Cuanta horas  con  minutos estuvo: ";cin>>hora>>min;
+        extra =(min>5)?c_u*1:c_u*0 ;
+        total=hora*c_u+extra;
+        cout<<"Total a pagar es: "<<total;
+    }
+    else if (dias[6]==dia || dias[5]==dia){
+        c_u=3;
+        cout<<"Cuantas horas con minutos estuvo:  ";cin>>hora>>min;
+        extra =(min>5)?c_u*1:c_u*0 ;
+        total=hora*c_u+extra;
+        cout<<"Total a pagar es: "<<total;
+    }
+    else{
+        c_u=2.5;
+        cout<<"Cuantas horas con min estuvo: ";cin>>hora>>min;
+        extra =(min>5)?c_u*1:c_u*0 ;
+        total=hora*c_u+extra;
+        cout<<"Total a pagar es: "<<total;
+    }
+}
