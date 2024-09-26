dae436ed0f54f70a9fc70db409c68eaec7b92aaf
Angelo <angeloquispesuarez@gmail.com>
2024-09-25 19:00:30 -0500

    Se suben las dos ultimas , no se hacer buenso commits

--- lab_1_practica/problema17.cpp
+++ lab_1_practica/problema17.cpp
@@ -2,3 +2,34 @@
 // que el menor valor puede repetirse. Por ejemplo: Si los números ingresados fueran: 14, 19, 14,
 // 16, 15. Los resultados deben ser: El menor valor ingresado fue 14 y corresponden al primer
 // número, tercer número.
+#include<iostream>
+#include<vector>
+using namespace std;
+void menor(int *vector,string puestos[5]){
+    int menor;
+    menor = vector[0];
+    for(int i=0;i<5;i++){
+        if(menor>vector[i]){
+            menor=vector[i];
+        }
+    }
+    cout<<"El termino menor es: "<<menor<<endl;
+    for(int i=0;i<5;i++){
+        if(menor==vector[i]){
+            cout<<"Corresponde al "<<puestos[i]<<" numero"<<endl;
+        }
+    }
+}
+int main (){
+    int *vector = new int[5];
+    string puestos[5]={"primer","segundo","tercer","cuarto","quinto"};
+    for(int i=0;i<5;i++){
+        int valor;
+        cout<<"Ingrese el termino "<<i+1<<" : ";cin>>valor;
+        cout<<endl;
+        *(vector+i)=valor;
+    }
+    menor(vector,puestos);
+    delete[] vector;
+    return 0;
+}
